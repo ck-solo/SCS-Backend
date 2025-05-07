268c754528912f40cf1e42417539d9198d0532ec
Lucy <youremail@example.com>
2025-05-07 13:57:19 +0530

    create cookie-pareser Know about cookie

--- Read&write file/Cookie-Parser/script.js
+++ Read&write file/Cookie-Parser/script.js
@@ -1,13 +1,21 @@
-const express = require('expres');
+const express = require('express');
 const app = express();
+const cookiePareser = require("cookie-parser")
 
-app.get("/"),function(req,res,next){
+
+app.use(cookiePareser());
+
+app.get("/",function(req,res,next){
     res.send("hey");
-}
+})
+app.get("/check",function(req,res,next){
+     console.log(req.cookies.name);
+     res.send("Checking")
+})
 
-app.get("/banned"),function(req,res,next){
-    res.cookie("banned","true");
+app.get("/banned",function(req,res,next){
+    res.cookie("name","utkarsh");
     res.send("Banned")
-}
+})
 
 app.listen(3000)
